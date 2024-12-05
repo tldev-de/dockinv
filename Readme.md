9a6d421bcb6961ab6393d5c2714dd79f73c0e023
tobias.dillig <tobias.dillig@frachtwerk.de>
2024-12-05 18:28:48 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# dock[er]inv[entory]
+
+## Description
+This project helps to get an overview of used docker images across multiple servers. It consists of two parts: dockinv agent and dockinv server. The agent is a simple python script that gives limited external access to the containers endpoint of the docker api. The server is a web application that collects the information and displays it in a web interface.
+
---
+++ agent/main.py
@@ -0,0 +1,63 @@
+import os
+import docker
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+DOCKER_SOCKET = os.getenv('DOCKER_SOCKET', '/var/run/docker.sock')
+HTTP_TOKEN = os.getenv('HTTP_TOKEN')
+HTTP_PORT = os.getenv('HTTP_PORT', 9999)
+
+
+@app.route('/containers', methods=['GET'])
+def get_docker_containers():
+    if HTTP_TOKEN:
+        token = request.headers.get('Authorization')
+        if token != HTTP_TOKEN:
+            return '403 forbidden', 403  # Forbidden
+
+    try:
+        client = docker.from_env()
+        containers = client.containers.list()
+        containers_info = [
+            {
+                'Id': container.id,
+                'Name': container.name,
+                'Image': container.attrs['Config']['Image'],
+                'Labels': container.labels,
+                'Status': container.status,
+                'StartedAt': container.attrs['State']['StartedAt'],
+            }
+            for container in containers
+        ]
+        return jsonify(containers_info)
+    except (docker.errors.APIError, docker.errors.DockerException) as e:
+        return jsonify({'error': str(e)}), 500
+@app.route('/health', methods=['GET'])
+def get_docker_containers():
+    if HTTP_TOKEN:
+        token = request.headers.get('Authorization')
+        if token != HTTP_TOKEN:
+            return '403 forbidden', 403  # Forbidden
+
+    try:
+        client = docker.from_env()
+        containers = client.containers.list()
+        containers_info = [
+            {
+                'Id': container.id,
+                'Name': container.name,
+                'Image': container.attrs['Config']['Image'],
+                'Labels': container.labels,
+                'Status': container.status,
+                'StartedAt': container.attrs['State']['StartedAt'],
+            }
+            for container in containers
+        ]
+        return jsonify(containers_info)
+    except (docker.errors.APIError, docker.errors.DockerException) as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=HTTP_PORT)
