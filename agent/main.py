be77712d3d94ec2d4c878c169205eb7c29855b52
tobias.dillig <tobias.dillig@frachtwerk.de>
2024-12-06 22:07:28 +0100

    feat: add healthcheck & non-root user in dockerfile

--- agent/main.py
+++ agent/main.py
@@ -32,31 +32,15 @@ def get_docker_containers():
         ]
         return jsonify(containers_info)
     except (docker.errors.APIError, docker.errors.DockerException) as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'status': 'error', 'message': str(e)}), 500
 @app.route('/health', methods=['GET'])
-def get_docker_containers():
-    if HTTP_TOKEN:
-        token = request.headers.get('Authorization')
-        if token != HTTP_TOKEN:
-            return '403 forbidden', 403  # Forbidden
-
+def get_health():
     try:
         client = docker.from_env()
-        containers = client.containers.list()
-        containers_info = [
-            {
-                'Id': container.id,
-                'Name': container.name,
-                'Image': container.attrs['Config']['Image'],
-                'Labels': container.labels,
-                'Status': container.status,
-                'StartedAt': container.attrs['State']['StartedAt'],
-            }
-            for container in containers
-        ]
-        return jsonify(containers_info)
+        client.version()
+        return jsonify({'status': 'ok', 'message': 'docker is accessible'})
     except (docker.errors.APIError, docker.errors.DockerException) as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'status': 'error', 'message': str(e)}), 500
 
 
 if __name__ == '__main__':
