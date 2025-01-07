f85bb9ba55a39b03e222e9e72af1009333ff414a
tobias.dillig <tobias.dillig@frachtwerk.de>
2025-01-07 23:18:38 +0100

    feat: add dev agent as host on start via docker-compose (relates #3)

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -5,4 +5,9 @@ if [ "$DATABASE_AUTO_UPGRADE" = 1 ]; then
   flask db upgrade
 fi
 
+if [ "$HOSTS_ADD_DEV_AGENT" = 1 ]; then
+  echo "add dev agent ..."
+  flask hosts add agent --address http://agent:9999/ --token your_secret
+fi
+
 exec "$@"
